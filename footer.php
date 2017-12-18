65729c75ed72afa822ef5f204d6acceb1ae7594b
jhung <jhung@ocadu.ca>
2017-12-18 16:05:58 -0500

    Added support for sidebar template fragments.

--- footer.php
+++ footer.php
@@ -4,8 +4,6 @@
  *
  * Contains the closing of the #content div and all content after.
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package a11y
  */
 
@@ -16,10 +14,9 @@
     <footer class="small-12 columns a11y-site-footer text-center" role="contentinfo">
         <p>
             <?php
-            $settings = (array) get_option ('a11y-theme-settings');
-            //$footer_string = esc_html($settings['site_text_footer']);
-            $footer_string = $settings['site_text_footer'];
-            echo $footer_string;
+           $settings = (array) get_option ('a11y-theme-settings');
+           $footer_string = $settings['site_text_footer'];
+           echo $footer_string;
             ?>
         </p>
 
