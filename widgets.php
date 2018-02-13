863b901437921b20edd256c85d92bee5a9ee0399
Eloisa <grrrero@users.noreply.github.com>
2018-02-13 16:29:52 -0500

    Changed headers in widgets to h2

--- widgets.php
+++ widgets.php
@@ -10,24 +10,24 @@ if (function_exists('register_sidebar')) {
 		'id'   => 'a11y-front-panel1',
 		'before_widget' => '<div id="%1$s" class="small-12 medium-4 columns a11y-front-panel">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="a11y-panel-header">',
-		'after_title'   => '</h1>'
+		'before_title'  => '<h2 class="a11y-panel-header">',
+		'after_title'   => '</h2>'
 	));
 	register_sidebar(array(
 		'name' => 'Front Panel 2',
 		'id'   => 'a11y-front-panel2',
 		'before_widget' => '<div id="%1$s" class="small-12 medium-4 columns a11y-front-panel">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="a11y-panel-header">',
-		'after_title'   => '</h1>'
+		'before_title'  => '<h2 class="a11y-panel-header">',
+		'after_title'   => '</h2>'
 	));
 	register_sidebar(array(
 		'name' => 'Front Panel 3',
 		'id'   => 'a11y-front-panel3',
 		'before_widget' => '<div id="%1$s" class="small-12 medium-4 columns a11y-front-panel">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="a11y-panel-header">',
-		'after_title'   => '</h1>'
+		'before_title'  => '<h2 class="a11y-panel-header">',
+		'after_title'   => '</h2>'
 	));
 }
 
